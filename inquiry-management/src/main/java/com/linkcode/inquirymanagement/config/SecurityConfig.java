52dbe9f6ac5919adfa53c744ef9538b048003f36
Sham1718 <119720894+Sham1718@users.noreply.github.com>
2026-09-03 13:32:28 +0530

    cors changed

--- inquiry-management/src/main/java/com/linkcode/inquirymanagement/config/SecurityConfig.java
+++ inquiry-management/src/main/java/com/linkcode/inquirymanagement/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:3000","https://inquirylinkcode.netlify.app/"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://enquiry-linkcode.vercel.app/"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
